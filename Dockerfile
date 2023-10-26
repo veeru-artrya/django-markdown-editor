437500be1959c9b3a48a26b4c5a314cbec498c4f
Veerendra B <veerendra.bodduluri@artrya.com>
2023-10-26 08:25:48 +0800

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine:3.16.0
-
-WORKDIR /app
+# FROM amazonlinux:2023.2.20231018.2
+COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.7.1 /lambda-adapter /opt/extensions/lambda-adapter
+WORKDIR /home
 
 RUN set -xe;
 
@@ -15,7 +16,12 @@ RUN apk add --no-cache python3 py3-pip tini; \
     adduser -u 1000 -G appuser -D -h /app appuser; \
     chown -R appuser:appuser /app
 
-USER appuser
+
+# USER appuser
+WORKDIR /home/martor_demo
+# RUN chmod 777 *
+RUN python3 manage.py collectstatic
 EXPOSE 8000/tcp
-ENTRYPOINT [ "tini", "--" ]
-CMD [ "python3", "/app/martor_demo/manage.py", "runserver", "0.0.0.0:8000" ]
+
+# ENTRYPOINT [ "tini", "--" ]
+CMD ["gunicorn", "martor_demo.wsgi:application", "-w", "1", "-b", "0.0.0.0:8000"]
--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# print the BASE_DIR
+print(f"BASE_DIR: {BASE_DIR}")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -141,7 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
-MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
+# MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
